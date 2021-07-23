6d2d85120ec2be1816cc46ac70ee29de3a6d1be2
Omar Hussein <ohussein@icon-creations.com>
2021-07-23 20:37:33 +0200

    Add Base Url and some major dependencies

--- app/src/main/java/com/icon/cleanarchmvvm/util/NetworkConnectivity.kt
+++ app/src/main/java/com/icon/cleanarchmvvm/util/NetworkConnectivity.kt
@@ -5,7 +5,7 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
 
-class NetworkHelper constructor(private val context: Context) {
+class NetworkConnectivity constructor(private val context: Context) {
 
     fun isNetworkConnected(): Boolean {
         var result = false
